11843649cf8930a07f3a5d836317e3a81b67efdb
Phanomphon Yotchon <phanomphon.yotchon@gmail.com>
2018-07-22 15:19:04 +0700

    Move to old

---
+++ download.js
@@ -0,0 +1,75 @@
+const http = require('http')
+const https = require('https')
+const fs = require('fs')
+const ProgressBar = require('progress')
+
+let arg_jsonin = process.argv[2]
+console.log('json input -> ' + arg_jsonin)
+
+let arg_ep = 1
+if (process.argv.length > 3){
+    arg_ep = process.argv[3]
+}
+console.log('ep -> ' + arg_ep)
+
+let arg_dir = '.'
+if (process.argv.length > 4){
+    arg_dir = process.argv[4]
+}
+console.log('save to path -> ' + arg_dir)
+
+let downloadFile = function (link, path) {
+    let file = fs.createWriteStream(path)
+    let url = new URL(link)
+    let h = http
+    if (url.protocol == "https:") {
+        h = https
+    }
+    console.log('start download file : ' + path)
+
+    let req = h.request(url)
+
+    req.on("response", function (response) {
+        response.pipe(file)
+
+        let len = parseInt(response.headers['content-length'], 10)
+        let bar = new ProgressBar('  downloading [:bar] :rate/bps :percent :etas', {
+            complete: '=',
+            incomplete: ' ',
+            width: 20,
+            total: len
+        });
+        let curr = 1
+        let len90 = null
+        if (isNaN(len)) {
+            len = 1024 * 1024 * 150
+            len90 = 1024 * 1024 * 120
+        }
+        response.on('data', function (chunk) {
+            curr += chunk.length
+            if (len90 != null && len90 > curr) {
+                bar.update(0.9)
+            } else {
+                bar.tick(chunk.length)
+            }
+        })
+
+        response.on('end', function () {
+            console.log('\n');
+        })
+
+        file.on('finish', function () {
+            file.close()
+            bar.update(1)
+        })
+    });
+    req.end()
+}
+
+fs.readFile(arg_jsonin, function (err, content) {
+    if (err) throw err;
+    let epLinkList = JSON.parse(content);
+    console.log('Max ep -> ' + epLinkList.length)
+
+    downloadFile(epLinkList[arg_ep - 1].videoLink, arg_dir + '/' + epLinkList[arg_ep - 1].filename + '.mp4')
+})
---
+++ getLink.js
@@ -0,0 +1,67 @@
+const request = require('sync-request')
+const {
+    JSDOM
+} = require('JSDOM')
+
+const fs = require('fs')
+
+var arg_url=process.argv[2] 
+console.log("url -> " + arg_url)
+var arg_out=process.argv[3]
+console.log("output -> " + arg_out)
+
+let createIframeUrl = function (vid, n) {
+    let currentdate = new Date()
+    let h = currentdate.getUTCHours()
+    let d = currentdate.getUTCDate()
+    let dh
+    let dh2
+    let m
+    if (h === 0) {
+        dh = d + '-' + h
+        dh2 = d + '-' + '24'
+    } else {
+        m = h - 1
+        dh = d + '-' + h
+        dh2 = d + '-' + m
+    }
+    return 'http://www.kseries.co/clip/play.php?id=' + vid + '&width=1005&height=540&dh=' + dh + '&dh2=' + dh2 + '&os=&n=' + n
+}
+
+let res = request('GET', arg_url)
+let body = res.getBody().toString()
+
+let dom = new JSDOM(body)
+let lstA = dom.window.document.querySelectorAll('a')
+let epLinkList = []
+for (let idx in lstA) {
+    if (lstA[idx].href !== undefined && lstA[idx].href.match('^http://www.kseries.co/clip/.+') != null) {
+        let link = lstA[idx].href
+        let fname = lstA[idx].textContent
+        let videoLink = ''
+
+        let vid = link.match('[0-9]+')[0]
+        for (let n = 0; n < 4; n++) {
+            let urlIframe = createIframeUrl(vid, n)
+            let resIframe = request('GET', urlIframe)
+            let bodyIframe = resIframe.getBody().toString()
+            let domIframe = new JSDOM(bodyIframe)
+            let lstVideo = domIframe.window.document.getElementsByTagName('source')
+            for (let idx in lstVideo) {
+                if (lstVideo[idx].src != undefined && !lstVideo[idx].src.match('www.kseries.co')) {
+                    videoLink = lstVideo[idx].src
+                    n = 5
+                    break
+                }
+            }
+        }
+        epLinkList.push({
+            filename: fname,
+            videoLink: videoLink
+        })
+    }
+}
+
+console.log(epLinkList)
+var json = JSON.stringify(epLinkList);
+fs.writeFile(arg_out, json, 'utf8', function (){ });
